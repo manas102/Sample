398b012118bc89b7ba7c7a1aaf217943f23c6d45
manas <macmanas4@gmail.com>
2019-07-26 10:03:57 +0530

    Greet class added

---
+++ src/sample/Greet.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class Greet {
+	private String name;
+	Greet(String str){
+		this.name=str;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
